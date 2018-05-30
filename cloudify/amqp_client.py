07780825fb1f21ac7cfc61b08aab99a143ef9d4c
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2018-05-30 12:28:42 +0200

    mandatory

--- cloudify/amqp_client.py
+++ cloudify/amqp_client.py
@@ -249,6 +249,7 @@ class AMQPConnection(object):
             # to the calling thread - see the publish method
             message = envelope['message']
             err_queue = envelope.get('err_queue')
+            message.setdefault('mandatory', True)
 
             try:
                 channel.publish(**message)
